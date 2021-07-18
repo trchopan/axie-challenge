205f4da4f65b172abbad1be4339853fd5bb2e881
Quang Tran (Chop Tr) <logan1011001@gmail.com>
2021-07-18 19:20:49 +0700

    Add Loading component

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -26,27 +26,7 @@
       <Icon v-if="largeIcon" :name="largeIcon" size="32px" />
     </div>
     <template v-else>
-      <svg
-        v-if="loading"
-        class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <circle
-          class="opacity-25"
-          cx="12"
-          cy="12"
-          r="10"
-          stroke="blue"
-          stroke-width="4"
-        ></circle>
-        <path
-          class="opacity-75"
-          fill="blue"
-          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-        ></path>
-      </svg>
+      <Loading v-if="loading" :color="primary ? 'white' : 'blue'" />
       <span
         class="button-label transition duration-150 ease-in-out"
         :class="{
@@ -92,14 +72,6 @@ export default defineComponent({
 
 <style lang="scss" scoped>
 .button {
-  svg {
-    circle {
-      stroke: blue;
-    }
-    path {
-      fill: blue;
-    }
-  }
   &-label {
     align-items: center;
     display: flex;
@@ -114,14 +86,6 @@ export default defineComponent({
   &-primary {
     background: linear-gradient(256.28deg, #1c94f4 0%, #1273ea 100%);
     color: #ffffff;
-    svg {
-      circle {
-        stroke: white;
-      }
-      path {
-        fill: white;
-      }
-    }
   }
   &-small {
     color: #57627b;
