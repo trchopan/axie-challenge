2278cecfc003187fc7964a02477ef18003f60600
Quang Tran (Chop Tr) <logan1011001@gmail.com>
2021-07-20 15:53:51 +0700

    Cleanup unuse entryPath application and store

--- src/application/entryPath.ts
+++
@@ -1,9 +0,0 @@
-import {InjectionKey, provide, inject, ref, Ref} from 'vue';
-
-const key: InjectionKey<Ref<string>> = Symbol();
-
-const entryPath = ref('');
-
-export const provideEntryPath = () => provide<Ref<string>>(key, entryPath);
-
-export const useEntryPath = () => inject(key, entryPath);
